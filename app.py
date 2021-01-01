423072522375733b43c258dac34b4f15496d2d33
AngelLopezBravo <74428331+AngelLopezBravo@users.noreply.github.com>
2021-01-01 10:55:37 +0100

    Ala, a otra cosa.

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 
-name = input("What is your name? ")
-age = input("How old are you? ")
+name = input('What is your name? ')
+age_c = input('How old are you? ')
+age_i = int(age_c)
 
-year = 2021 - int(age)
+year = 2021 - int(age_i)
 
-print("Hello " + name + ". You where born at " + str(year))
+print('Hello ' + name + '. You where born at ' + str(year))
+
+
+if age_i > 10:
+    print('Menor de 10')
+else:
+    print('Mayor de 10')
+
+print('Fin')
