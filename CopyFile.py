f54cccd90d7b72f6480364b29f9f484ffecddabe
AngelLopezBravo <74428331+AngelLopezBravo@users.noreply.github.com>
2021-01-31 22:28:46 +0100

    Copy File.

--- CopyFile.py
+++ CopyFile.py
@@ -3,3 +3,22 @@ import shutil
 origen = r'c:\temp\origen.txt'
 destino = r'c:\temp\destino.txt'
 shutil.copyfile(origen,destino)
+
+
+Vertical  = input('Indique Vertical: ')
+Equipo = input('Indique Nombre del Equipo: ')
+
+archivo = open(destino, "r")
+datos = archivo.read()
+print('datos; ', datos)
+
+datos = datos.replace('<Vertical>', Vertical)
+datos = datos.replace('<Nombre del equipo>', Equipo)
+
+archivo = open(destino, "w")
+archivo.write(datos)
+archivo.close()
+
+archivo = open(destino, "r")
+print('datos; ', archivo.read())
+
